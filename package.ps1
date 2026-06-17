fea832c64201525ee081bb8d508ddae25aa2ce3f
Alex Conticello <alexconticello@gmail.com>
2026-06-16 20:52:44 -0400

    Fix packaging script.

--- package.ps1
+++ package.ps1
@@ -7,25 +7,26 @@ param(
 $ErrorActionPreference = "Stop"
 
 $distDir   = Join-Path $SolutionDir "dist"
-$nvseDir   = Join-Path $distDir "nvse"
-$pluginDir = Join-Path $nvseDir "plugins"
+$nvseOutDir   = Join-Path $distDir "nvse"
+$pluginOutDir = Join-Path $nvseOutDir "plugins"
+$configOutDir   = Join-Path $distDir "config"
 
 $nvseSrcDir = Join-Path $SolutionDir "nvse"
-$configDir  = Join-Path $SolutionDir "config"
+$configSrcDir  = Join-Path $SolutionDir "config"
 
 # Create folder structure
-New-Item -ItemType Directory -Force -Path $pluginDir | Out-Null
+New-Item -ItemType Directory -Force -Path $pluginOutDir | Out-Null
 
 # --- 1. Plugin ZIP (DLL + PDB) ---
-Copy-Item -Path (Join-Path $TargetDir "$TargetName.dll") -Destination $pluginDir -Force
-Copy-Item -Path (Join-Path $TargetDir "$TargetName.pdb") -Destination $pluginDir -Force
+Copy-Item -Path (Join-Path $TargetDir "$TargetName.dll") -Destination $pluginOutDir -Force
+Copy-Item -Path (Join-Path $TargetDir "$TargetName.pdb") -Destination $pluginOutDir -Force
 
 # --- 2. Script runner source and config  ---
-Copy-Item -Path $nvseSrcDir/* -Destination $nvseDir -Force -Recurse
-Copy-Item -Path $configDir -Destination $distDir -Force -Recurse
+Copy-Item -Path $nvseSrcDir/* -Destination $nvseOutDir -Force -Recurse
+Copy-Item -Path $configSrcDir -Destination $distDir -Force -Recurse
 
 Compress-Archive -Path $distDir/* -DestinationPath (Join-Path $distDir "$TargetName.zip") -Force
 
 # Clear temp folder
-Remove-Item -Path $nvseDir -Force -Recurse
-Remove-Item -Path $configDir -Force -Recurse
+Remove-Item -Path $nvseOutDir -Force -Recurse
+Remove-Item -Path $configOutDir -Force -Recurse
